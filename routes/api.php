f36de5b9df340e50b3406510c8df59d9ccaae528
Nurulhamidaah <nurulhamidah12455@gmail.com>
2023-02-16 14:56:48 +0700

    Auth

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,20 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\AuthC;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostC;
+use Illuminate\Support\Facades\Hash;
 
 Route::get('/', function () {
     return 'Hello World API !';
 });
 
-Route::get('/posts', [PostC::class, 'index']);
-Route::get('/posts/{id}', [PostC::class, 'detail']);
+Route::get('/posts', [PostC::class, 'index'])->middleware(['auth:sanctum']);
+Route::get('/posts/{id}', [PostC::class, 'detail'])->middleware(['auth:sanctum']);
+
+Route::post('/login', [AuthC::class, 'login']);
+
+Route::get('/password', function(){
+    return Hash::make('rahasia');
+});
