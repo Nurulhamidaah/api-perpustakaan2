2114b7ed39b60eb39ee3ba65030dac30d6267ada
Nurulhamidaah <nurulhamidah12455@gmail.com>
2023-02-16 11:24:57 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostC;
+
+Route::get('/', function () {
+    return 'Hello World API !';
+});
+
+Route::get('/posts', [PostC::class, 'index']);
+Route::get('/posts/{id}', [PostC::class, 'detail']);
